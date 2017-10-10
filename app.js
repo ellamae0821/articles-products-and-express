8594ab196009d2b2962f709908410dab2e76b9b5
Ella Mae A <ellamae207@gmail.com>
2017-10-09 14:31:01 -1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+const express = require('express');
+const exphbs = require ('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require ('method-override');
+const PORT = process.env.PORT|| 8080;
+const app = express();
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+
+
+app.set('view engine', '.hbs');
+
+
+app.get('/', (req, res) =>{
+  res.render ();
+});
+
+app.listen(PORT, () =>{
+  console.log('Sever is listening to ', PORT);
+});
